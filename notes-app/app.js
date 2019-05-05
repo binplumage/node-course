760cc9c7f9571be66b18c361a90093c9a9a82259
plumage <balaja2179@gmail.com>
2019-05-05 10:10:22 -0400

    feat: install yargs and add four command(add, remove, list, read)

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,48 @@
-const fs = require('fs');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-fs.writeFileSync('notes.txt', 'Create note by nodejs!');
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Note: ' + argv.body );
+    }
+})
 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function() {
+        console.log('Removing the note');
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'list all notes',
+    handler: function() {
+        console.log('List all notes');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'read the note',
+    handler: function() {
+        console.log('Read the node');
+    }
+})
+console.log(yargs.argv);
