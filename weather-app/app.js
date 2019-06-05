63fa1581a6d3010a1f1ce90056c0f3b0c30cbb56
plumage <balaja2179@gmail.com>
2019-06-05 09:43:16 -0400

    feat: give address and call geocode and forecast util to get       weather forecast

---
+++ weather-app/app.js
@@ -0,0 +1,18 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+geocode('New Taipei', (error, data) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log(data.location)
+        console.log(forecastData)
+      })
+})
+
