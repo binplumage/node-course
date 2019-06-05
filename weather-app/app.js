5b726a3f2254f5b89fe980ee3b018c06e684a14a
plumage <balaja2179@gmail.com>
2019-06-05 10:03:44 -0400

    feat: get address from argv

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,24 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('New Taipei', (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
+const address = process.argv[2]
 
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
         if (error) {
             return console.log(error)
         }
+        console.log(data)
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
 
-        console.log(data.location)
-        console.log(forecastData)
-      })
-})
+            console.log(data.location)
+            console.log(forecastData)
+          })
+    })
+}
 
