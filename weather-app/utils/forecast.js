fb10406b38ef0a0acde42fcc382a10d1b50b2e9e
plumage <balaja2179@gmail.com>
2019-06-09 03:12:32 -0400

    refactor: modify object by destructuring and property shorthand

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/59d5184149f0afbc6ed906a0a2bdabf2/' + latitude + ',' + longitude + '?lang=zh-tw'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect weather service!')
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find lcoation.')
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
