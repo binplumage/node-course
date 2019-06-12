26fa1f00f9c951609797fad18e8a2d7391fb8dfe
plumage <balaja2179@gmail.com>
2019-06-12 10:31:25 -0400

    feat: use handlebars to render dynamic content

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,34 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Plumage'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'plumage'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help'
+    })
+})
+app.get('/weather', (req, res) => {
+    res.send({
+        forcast: 'sunny',
+        location: 'Taiwan'
+    })
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
