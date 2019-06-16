349792efb4be852c05a7a7cc6546873c9ad1fad5
plumage <balaja2179@gmail.com>
2019-06-16 03:45:48 -0400

    feat: add 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -44,6 +44,22 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Plumage',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Plumage',
+        errorMessage: 'Page not found'
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
