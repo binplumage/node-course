358fcfab410c9fea2311d1f1d971d72313534aff
plumage <balaja2179@gmail.com>
2019-06-17 15:52:45 +0800

    feat: Add forecast and geocode util and get the forecast by 	  address

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -38,9 +40,27 @@ app.get('/help', (req, res) => {
     })
 })
 app.get('/weather', (req, res) => {
-    res.send({
-        forcast: 'sunny',
-        location: 'Taiwan'
+	if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error: error })
+        }
+        forecast(latitude, longitude, (error, forcastData) => {
+            if (error) {
+                return res.send({ error: error})
+            }
+
+            res.send({
+                address: req.query.address,
+                location,
+                forcast: forcastData
+            })
+        })
     })
 })
 
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/59d5184149f0afbc6ed906a0a2bdabf2/' + latitude + ',' + longitude + '?lang=zh-tw'
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect weather service!')
+        } else if (body.error) {
+            callback('Unable to find lcoation.')
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
